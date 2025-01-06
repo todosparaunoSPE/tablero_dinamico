b2474e1f5cd59cb9ae950a7bb615aa52179421a9
todosparaunoSPE <148250432+todosparaunoSPE@users.noreply.github.com>
2025-01-06 11:23:06 -0600

    Update app2.py

--- app2.py
+++ app2.py
@@ -14,6 +14,25 @@ import time
 from sklearn.linear_model import LinearRegression
 from sklearn.ensemble import RandomForestRegressor
 
+# Estilo de fondo
+page_bg_img = """
+<style>
+[data-testid="stAppViewContainer"]{
+background:
+radial-gradient(black 15%, transparent 16%) 0 0,
+radial-gradient(black 15%, transparent 16%) 8px 8px,
+radial-gradient(rgba(255,255,255,.1) 15%, transparent 20%) 0 1px,
+radial-gradient(rgba(255,255,255,.1) 15%, transparent 20%) 8px 9px;
+background-color:#282828;
+background-size:16px 16px;
+</style>
+"""
+
+st.markdown(page_bg_img, unsafe_allow_html=True)
+
+
+
+
 # Configuración de la página
 st.set_page_config(page_title="Tablero Multiusuario", layout="wide")
 
